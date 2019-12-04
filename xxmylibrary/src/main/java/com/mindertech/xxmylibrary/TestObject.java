6f9b412cf9d4ad6898a28302e58b229da50ff45f
xiangxia <xia.xiang@mindertech.co.uk>
2019-12-04 13:15:19 +0800

    Initial commit

---
+++ xxmylibrary/src/main/java/com/mindertech/xxmylibrary/TestObject.java
@@ -0,0 +1,15 @@
+package com.mindertech.xxmylibrary;
+
+/**
+ * @project testmodule
+ * @package：com.mindertech.xxmylibrary
+ * @anthor xiangxia
+ * @time 2019-12-04 13:11
+ * @description 描述
+ */
+public class TestObject {
+
+    public static String getString() {
+        return "my library";
+    }
+}
